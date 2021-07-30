8d3b6be2db94e18ba2bd790867f1c8748603ca7e
asmithasatesh <asmithasatesh@gmail.com>
2021-07-30 11:49:23 +0530

    Create table in Database

--- Emploee Payroll.sql
+++ Emploee Payroll.sql
@@ -1,3 +1,10 @@
 ------- UC 1: Create Database -------
 create database payroll_services
 use payroll_services
+
+------- UC 2: Create Table -------
+Create Table employee_payroll
+(EmployeeID int identity(1,1) primary key,
+EmployeeName varchar(100),
+Salary float,
+StartDate Date)
