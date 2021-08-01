e30d97bfdcb020743b31942244b51722c3e0feac
asmithasatesh <asmithasatesh@gmail.com>
2021-08-01 15:20:32 +0530

    [Asmitha]Redo Usecase 4,5 andd 7 using ER Diagram Implementation

--- Emploee Payroll.sql
+++ Emploee Payroll.sql
@@ -171,6 +171,7 @@ insert into EmployeeDepartment values
 select * from EmployeeDepartment
 
 ------- UC 12: Ensure all retrieve queries done especially in UC 4, UC 5 and UC 7 are working with new table structure -------
+
 --UC 4: Retrieve all Data
 SELECT CompanyID,CompanyName,EmployeeID,EmployeeName,EmployeeAddress,EmployeePhoneNumber,StartDate,Gender,BasicPay,Deductions,TaxablePay,IncomeTax,NetPay,DepartName
 FROM Company
@@ -179,19 +180,38 @@ INNER JOIN PayrollCalculate on PayrollCalculate.EmployeeIdentity=Employee.Employ
 INNER JOIN EmployeeDepartment on Employee.EmployeeID=EmployeeDepartment.EmployeeIdentity
 INNER JOIN Department on Department.DepartmentId=EmployeeDepartment.DepartmentIdentity
 
-SELECT CompanyID,CompanyName,EmployeeID,EmployeeName,BasicPay,Deductions,TaxablePay,IncomeTax,NetPay,DepartName
+--UC 5: Select Query using Cast() an GetDate()
+SELECT CompanyID,CompanyName,EmployeeID,EmployeeName,BasicPay,Deductions,TaxablePay,IncomeTax,NetPay
 FROM Company
-INNER JOIN Employee ON Company.CompanyID = Employee.CompanyIdentity and StartDate BETWEEN Cast('2015-11-12' as Date) and GetDate()
+INNER JOIN Employee ON Company.CompanyID = Employee.CompanyIdentity and StartDate BETWEEN Cast('2012-11-12' as Date) and GetDate()
 INNER JOIN PayrollCalculate on PayrollCalculate.EmployeeIdentity=Employee.EmployeeID
-INNER JOIN EmployeeDepartment on Employee.EmployeeID=EmployeeDepartment.EmployeeIdentity
-INNER JOIN Department on Department.DepartmentId=EmployeeDepartment.DepartmentIdentity
+--Retrieve query based on Name
+SELECT CompanyID,CompanyName,EmployeeID,EmployeeName,BasicPay,Deductions,TaxablePay,IncomeTax,NetPay
+FROM Company
+INNER JOIN Employee ON Company.CompanyID = Employee.CompanyIdentity and Employee.EmployeeName='Kriti Deshmuk'
+INNER JOIN PayrollCalculate on PayrollCalculate.EmployeeIdentity=Employee.EmployeeID
 
-SELECT CompanyID,CompanyName,EmployeeID,EmployeeName,BasicPay,Deductions,TaxablePay,IncomeTax,NetPay,DepartName
-FROM Company
-INNER JOIN Employee ON Company.CompanyID = Employee.CompanyIdentity and Employee.EmployeeName='Sarang Nair'
-INNER JOIN PayrollCalculate on PayrollCalculate.EmployeeIdentity=Employee.EmployeeID
-INNER JOIN EmployeeDepartment on Employee.EmployeeID=EmployeeDepartment.EmployeeIdentity
-INNER JOIN Department on Department.DepartmentId=EmployeeDepartment.DepartmentIdentity
+--UC 7: Use Aggregate Functions and Group by Gender
+
+select Sum(BasicPay) as "TotalSalary",Gender 
+from Employee
+INNER JOIN PayrollCalculate on PayrollCalculate.EmployeeIdentity=Employee.EmployeeID group by Gender;
+
+select Avg(BasicPay) as "AverageSalary",Gender 
+from Employee
+INNER JOIN PayrollCalculate on PayrollCalculate.EmployeeIdentity=Employee.EmployeeID group by Gender;
+
+select Min(BasicPay) as "MinimumSalary",Gender 
+from Employee
+INNER JOIN PayrollCalculate on PayrollCalculate.EmployeeIdentity=Employee.EmployeeID group by Gender;
+
+select Max(BasicPay)  as "MaximumSalary",Gender 
+from Employee
+INNER JOIN PayrollCalculate on PayrollCalculate.EmployeeIdentity=Employee.EmployeeID group by Gender;
+
+select Count(BasicPay) as "CountSalary",Gender 
+from Employee
+INNER JOIN PayrollCalculate on PayrollCalculate.EmployeeIdentity=Employee.EmployeeID group by Gender;
 
 
 
